18b0e0a71ca083da54aba4a3518ef1aa4c69ee6c
MilkyWay34 <113975033+MilkyWay34@users.noreply.github.com>
2023-02-02 21:42:01 +0300

    module12-task2

--- js/util.js
+++ js/util.js
@@ -31,10 +31,19 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
 export {
   getRandomPositiveInteger,
   checkStringLength,
   getRandomArrayElement,
   showAlert,
+  debounce,
 };
 
